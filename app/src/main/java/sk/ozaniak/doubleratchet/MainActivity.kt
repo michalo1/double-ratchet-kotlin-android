0aae2fc14df2a1ec3dfa1383c6ea1c0f245545eb
Michal Ozaniak <ozaniak.michal@gmail.com>
2017-12-18 15:26:35 +0100

    Project created

---
+++ app/src/main/java/sk/ozaniak/doubleratchet/MainActivity.kt
@@ -0,0 +1,12 @@
+package sk.ozaniak.doubleratchet
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
